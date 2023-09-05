c8963445267c77379609967788ce9c77a334ccf7
tigricina <karina6891@mail.ru>
2023-09-06 02:17:21 +0300

    change css style to css module

--- hacker-news/src/NewsItem/NewsItem.js
+++ hacker-news/src/NewsItem/NewsItem.js
@@ -1,16 +1,16 @@
-import './NewsItem.css'
+import style from './NewsItem.module.css'
 
 export function NewsItem() {
     return (
-        <div className='container'>
-            <a className='link' href="example.com">Title</a>
-            <div className='info'>
-                <div className='userData'>
+        <div className={style.container}>
+            <a className={style.link} href="example.com">Title</a>
+            <div className={style.info}>
+                <div className={style.userData}>
                     <span>username | </span>
                     <span>00:00:00</span>
                 </div>
 
-                <div className='score'>
+                <div className={style.score}>
                     1 points
                 </div>
             </div>
