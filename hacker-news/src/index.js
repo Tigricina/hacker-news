fed210f4642be9651f2d2bb33c2a996e70563120
tigricina <karina6891@mail.ru>
2023-09-07 01:35:08 +0300

    get parameters to component

--- hacker-news/src/index.js
+++ hacker-news/src/index.js
@@ -6,16 +6,31 @@ import {NewsItem} from "./NewsItem/NewsItem";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  
+  <>
     <NewsItem 
       title='Первая новость' 
       url='www.example.com' 
       username='Пользователь' 
       date='10.10.11' 
       score={10}
+    />
+    <NewsItem 
+      title='Вторая новость' 
+      url='www.example.com' 
+      username='Пользователь 2' 
+      date='10.20.11' 
+      score={100}
+    />
+    <NewsItem 
+      title='Третья новость' 
+      url='www.example.com' 
+      username='Пользователь 3' 
+      date='10.10.11' 
+      score={20}
+    />
+  </>
+  
 
-    >text
-    </NewsItem>
 
 
   
