5e3c92e2326f677744a792c54f503eff7ae98c9d
tigricina <karina6891@mail.ru>
2023-09-07 02:29:49 +0300

    keys in React

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,8 +1,50 @@
+import { NewsItem } from "./NewsItem/NewsItem"
+
+const news = [
+  {
+    title: 'Первая новость',
+    url: 'www.example.com',
+    username: 'Пользователь',
+    date: '11.11.11',
+    score: 100,
+    id: '1'
+  },
+  {
+    title: 'Вторая новость',
+    url: 'www.example.com',
+    username: 'Пользователь 2',
+    date: '05.10.11',
+    score: 1000,
+    id: '2'
+  },
+  {
+    title: 'Третья новость',
+    url: 'www.example.com',
+    username: 'Пользователь 3',
+    date: '11.09.11',
+    score: 200,
+    id: '3'
+  }
+]
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      {
+        news.map(item => {
+          return (
+            <NewsItem 
+              title={item.title} 
+              url={item.url} 
+              username={item.username} 
+              date={item.date}
+              score={item.score}
+              key={item.id}
+            />
+          )
+        })
+      }
+    </>
   );
 }
 
--- hacker-news/src/index.js
+++ hacker-news/src/index.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {NewsItem} from "./NewsItem/NewsItem";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <>
-    <NewsItem 
-      title='Первая новость' 
-      url='www.example.com' 
-      username='Пользователь' 
-      date='10.10.11' 
-      score={10}
-    />
-    <NewsItem 
-      title='Вторая новость' 
-      url='www.example.com' 
-      username='Пользователь 2' 
-      date='10.20.11' 
-      score={100}
-    />
-    <NewsItem 
-      title='Третья новость' 
-      url='www.example.com' 
-      username='Пользователь 3' 
-      date='10.10.11' 
-      score={20}
-    />
-  </>
-  
-
-
-
-  
+    <App/>  
 );
 
 
