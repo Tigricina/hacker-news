71ae3c0e122a320c83633920f903298edeab8d56
tigricina <karina6891@mail.ru>
2023-10-08 23:26:26 +0300

    added hacker-news api

--- hacker-news/src/NewsItem/NewsItem.js
+++ hacker-news/src/NewsItem/NewsItem.js
@@ -1,20 +1,29 @@
-import style from './NewsItem.module.css'
+import style from "./NewsItem.module.css";
+import { unixToDate } from "../utils/utils";
 
 export function NewsItem(props) {
-    console.log(props.children)
-    return (
-        <div className={style.container}>
-            <a className={style.link} href="example.com">{props.title}</a>
-            <div className={style.info}>
-                <div className={style.userData}>
-                    <span>{props.username} | </span>
-                    <span>{props.date}</span>
-                </div>
+  const scoreClassArr = [style.score];
 
-                <div className={style.score}>
-                {props.score} points
-                </div>
-            </div>
+  if (props.score > 50) {
+    scoreClassArr.push(style.highScore);
+  } else if (props.score > 30) {
+    scoreClassArr.push(style.midScore);
+  } else {
+    scoreClassArr.push(style.lowScore);
+  }
+  return (
+    <div className={style.container}>
+      <a className={style.link} href="example.com">
+        {props.title}
+      </a>
+      <div className={style.info}>
+        <div className={style.userData}>
+          <span>{props.username} | </span>
+          <span>{unixToDate(props.date)}</span>
         </div>
-    )
+
+        <div className={scoreClassArr.join(" ")}>{props.score} points</div>
+      </div>
+    </div>
+  );
 }
